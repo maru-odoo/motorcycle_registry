632ba66d07f72692ed7b7cdb45a8d7ac672a69c2
Matthew Rubino <maru@odoo.com>
2023-06-13 10:58:28 -0400

    [TE02] Create a Module

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'description': """Motorcycle Registry
+    ====================
+    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.""",
+    'author': 'maru-odoo',
+    'website': 'https://github.com/maru-odoo/motorcycle_registry',
+    'category': 'Kawiil/Kawiil',
+    'depends': ['base'],
+    'application': True,
+}
