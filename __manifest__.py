963ff8aa322c6a154b6265c8417c46ab6c39205e
Matthew Rubino <maru@odoo.com>
2023-06-14 13:36:02 -0400

    [TE07] Actions and Menus

--- __manifest__.py
+++ __manifest__.py
@@ -11,6 +11,8 @@
     'data': [
         'security/motorcycle_registry_groups.xml',
         'security/ir.model.access.csv',
+        'views/motorcycle_registry_actions.xml',
+        'views/motorcycle_registry_menuitems.xml',
     ],
     'demo': [
         'demo/motorcycle_registry_demo.xml',
