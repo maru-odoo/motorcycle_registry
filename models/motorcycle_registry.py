fbbf51444f1e125f6fdadaf132f0763c55eff0e9
Matthew Rubino <maru@odoo.com>
2023-06-15 11:35:22 -0400

    [TE10] Advanced Fields

--- models/motorcycle_registry.py
+++ models/motorcycle_registry.py
@@ -11,13 +11,32 @@ class MotorcycleRegistry(models.Model):
 
     registry_number = fields.Char(default="MRN0000", copy=False, required=True, readonly=True)
     vin = fields.Char(required=True)
-    first_name = fields.Char(required=True)
-    last_name = fields.Char(required=True)
+    make = fields.Char(compute="_compute_make")
+    model = fields.Char(compute="_compute_model")
+    year = fields.Char(compute="_compute_year")
+    owner_id = fields.Many2one(comodel_name="res.partner", string="Owner")
+    phone = fields.Char(related="owner_id.phone")
+    email = fields.Char(related="owner_id.email")
     picture = fields.Image()
     current_mileage = fields.Float()
     license_plate = fields.Char()
     certificate_title = fields.Binary()
     register_date = fields.Date()
+
+    @api.depends("vin")
+    def _compute_make(self):
+        for record in self:
+            record.make = record.vin[0:2]
+
+    @api.depends("vin")
+    def _compute_model(self):
+        for record in self:
+            record.model = record.vin[2:4]
+
+    @api.depends("vin")
+    def _compute_year(self):
+        for record in self:
+            record.year = record.vin[4:6]
 
     @api.model_create_multi
     def create(self, vals_list):
